ad91d2a618937d92a49baa4755c3f1ae9d398dcf
Keerti <keertiagarwal2003@gmail.com>
2024-06-30 17:09:04 +0530

    new file

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import { AppProvider } from './context';
+import './index.css';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
+
+function App() {
+  return (
+    <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "/about" element = {<About />} />
+          <Route path = "/book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
+  )
+}
+
+export default App
